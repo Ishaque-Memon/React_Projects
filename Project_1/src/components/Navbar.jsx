5e0d1c00fd6d494b93cb0f5036279492513e9331
Muhammad Ishaque <haseebmeymon123@gmail.com>
2024-11-16 21:35:55 +0500

    Hero and Navbar Components Developed

--- Project_1/src/components/Navbar.jsx
+++ Project_1/src/components/Navbar.jsx
@@ -5,21 +5,21 @@ const Navbar = () => {
   return (
     <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
     <nav className='flex w-full screen-max-width'>
-        <img src={appleImg} alt="Apple" width={14} height={18} className="transition-all duration-300 ease-in-out hover:brightness-150 hover:scale-110 hover:shadow-glow cursor-pointer"/>
+        <img src={appleImg} alt="Apple" width={14} height={18} className="transition-all duration-300 ease-in-out hover:brightness-150 hover:scale-150 hover:shadow-glow cursor-pointer"/>
         <div className='flex flex-1 justify-center '>
             {
                 navLists.map(
                     (nav) => (
-                        <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white'>
+                        <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
                             {nav}
                         </div>
                     ))
                 
             }
         </div>
-        <div>
-            <img src={searchImg} alt='search' width={18} height={18}/>
-            <img src={bagImg} alt='bag' width={18} height={18}/>
+        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1 cursor-pointer'>
+            <img src={searchImg} alt='search' width={18} height={18} className='ttransition-all duration-300 ease-in-out hover:scale-150'/>
+            <img src={bagImg} alt='bag' width={18} height={18}  className='transition-all duration-300 ease-in-out hover:scale-150' />
         </div>
         
       </nav>
